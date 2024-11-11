f8fdd334388b0fc99023ac396d9fbf93577b406b
Navira Gayatri <141285340+gnavira@users.noreply.github.com>
2024-11-11 15:16:21 +0000

    Uploda new file:   firstrun/1/start.js new file:   firstrun/10/start.js new file:   firstrun/11/start.js new file:   firstrun/12/start.js new file:   firstrun/13/start.js new file:   firstrun/14/start.js new file:   firstrun/15/start.js new file:   firstrun/16/start.js new file:   firstrun/17/start.js new file:   firstrun/18/start.js new file:   firstrun/19/start.js new file:   firstrun/2/start.js new file:   firstrun/20/start.js new file:   firstrun/21/start.js new file:   firstrun/22/start.js new file:   firstrun/23/start.js new file:   firstrun/24/start.js new file:   firstrun/25/start.js new file:   firstrun/26/start.js new file:   firstrun/27/start.js new file:   firstrun/28/start.js new file:   firstrun/29/start.js new file:   firstrun/3/start.js new file:   firstrun/30/start.js new file:   firstrun/31/start.js new file:   firstrun/32/start.js new file:   firstrun/33/start.js new file:   firstrun/34/start.js new file:   firstrun/35/start.js new file:   firstrun/36/start.js new file:   firstrun/37/start.js new file:   firstrun/38/start.js new file:   firstrun/4/start.js new file:   firstrun/5/start.js new file:   firstrun/6/start.js new file:   firstrun/7/start.js new file:   firstrun/8/start.js new file:   firstrun/9/start.js new file:   notrun/1/start.js new file:   notrun/10/start.js new file:   notrun/11/start.js new file:   notrun/12/start.js new file:   notrun/13/start.js new file:   notrun/14/start.js new file:   notrun/15/start.js new file:   notrun/16/start.js new file:   notrun/17/start.js new file:   notrun/18/start.js new file:   notrun/19/start.js new file:   notrun/2/start.js new file:   notrun/20/start.js new file:   notrun/21/start.js new file:   notrun/22/start.js new file:   notrun/23/start.js new file:   notrun/24/start.js new file:   notrun/25/start.js new file:   notrun/26/start.js new file:   notrun/27/start.js new file:   notrun/28/start.js new file:   notrun/29/start.js new file:   notrun/3/start.js new file:   notrun/30/start.js new file:   notrun/31/start.js new file:   notrun/32/start.js new file:   notrun/33/start.js new file:   notrun/34/start.js new file:   notrun/35/start.js new file:   notrun/36/start.js new file:   notrun/37/start.js new file:   notrun/38/start.js new file:   notrun/4/start.js new file:   notrun/5/start.js new file:   notrun/6/start.js new file:   notrun/7/start.js new file:   notrun/8/start.js new file:   notrun/9/start.js

---
+++ firstrun/19/start.js
@@ -0,0 +1,292 @@
+const { ethers } = require('ethers');
+const chains = require('./chains');
+const provider = chains.mainnet.taiko.provider();
+const changeProvider = chains.mainnet.taiko.changeRpcProvider();
+const explorer = chains.mainnet.taiko.explorer;
+const changeRpc = chains.mainnet.tempTaiko.changeRpcProvider();
+const tempProvider = chains.mainnet.tempTaiko.provider();
+const fs = require('fs');
+const moment = require('moment-timezone');
+const { displayHeader, delay } = require('./chains/utils/utils');
+const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
+const { ABI, SEND_ABI } = require('./abi/abi');
+const WETH_CA = '0xA51894664A773981C6C112C43ce576f315d5b1B6';
+const SEND_CA = '0x2A5b0a407828b6Ca2E87e2e568CD8413fd5c24A1';
+const recipientsaddress = JSON.parse(fs.readFileSync('recipients.json', 'utf8'));
+const { CronJob } = require('cron');
+const amountCheck = ethers.parseEther('1', 'ether');
+const gasPrice = ethers.parseUnits('0.19', 'gwei');
+
+function appendLog(message) {
+  fs.appendFileSync('log.txt', message + '\n');
+}
+
+function isTimeoutError(error) {
+  return error.message.includes('504 Gateway Timeout') || 
+         error.message.includes('request timeout') || 
+         error.message.includes('failed to detect network') ||
+         error.message.includes('free limit') || 
+         error.message.includes('constant variable');
+}
+async function checkWethBalance(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, tempProvider);
+  const address = await wallet.getAddress();
+  let balanceWeth = await new ethers.Contract(WETH_CA, ABI, tempProvider).balanceOf(address);
+
+  const loadingSymbols = ['|', '/', '-', '\\'];
+  let index = 0;
+  const loadingInterval = setInterval(() => {
+    process.stdout.write(`\rChecking WETH balance for ${address}... ${loadingSymbols[index]}`);
+    index = (index + 1) % loadingSymbols.length;
+  }, 200);
+
+  const amountToUnwrap = ethers.parseUnits('1', 'ether')
+
+  while (balanceWeth <= amountToUnwrap) {
+    try {
+      await delay(5000);
+      balanceWeth = await new ethers.Contract(WETH_CA, ABI, tempProvider).balanceOf(address);
+    } catch (error) {
+      if (isTimeoutError(error)) {
+        console.log(`Kesalahan timeout terjadi: ${error.message}`);
+        await changeRpc;
+        continue;
+      }
+      const errorMessage = `Kesalahan memeriksa saldo: ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      throw error;
+    }
+  }
+  clearInterval(loadingInterval);
+  process.stdout.write('\r');
+  console.log(`${ethers.formatEther(balanceWeth)} WETH`.blue);
+  return balanceWeth;
+}
+
+async function doWrap(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const amount = ethers.parseUnits('1.5', 'ether');
+  const maxRetries = 3;
+  let attempt = 0;
+
+  while (attempt < maxRetries) {
+    try {
+      const wrapContract = new ethers.Contract(WETH_CA, ABI, wallet);
+      const txWrap = await wrapContract.deposit({ value: amount, gasPrice: gasPrice });
+      const receipt = await txWrap.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Wrap transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+		  error.message.includes('failed to detect network') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Wrap transaction.`);
+}
+async function doUnwrap(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const amount = ethers.parseUnits('1.5', 'ether');
+  const maxRetries = 3;
+  let attempt = 0;
+
+  while (attempt < maxRetries) {
+    try {
+      const unwrapContract = new ethers.Contract(WETH_CA, ABI, wallet);
+      const txUnwrap = await unwrapContract.withdraw(amount, { gasPrice: gasPrice });
+      const receipt = await txUnwrap.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Unwrap transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+		  error.message.includes('failed to detect network') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Unwrap transaction.`);
+}
+
+async function doSendEther(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const recipients = recipientsaddress;
+  const values = recipients.map(() => ethers.parseUnits('1.5', 'ether'));
+  const sendContract = new ethers.Contract(SEND_CA, SEND_ABI, wallet);
+  const maxRetries = 3;
+  let attempt = 0;
+
+  while (attempt < maxRetries) {
+    try {
+      const txSendContract = await sendContract.multicall(recipients, values, { value: ethers.parseUnits('1.5', 'ether'), gasPrice: gasPrice });
+      const receipt = await txSendContract.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Send ETH transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+		  error.message.includes('failed to detect network') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Send ETH transaction.`);
+}
+async function checkBalance(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, tempProvider);
+  const address = await wallet.getAddress();
+  let balance = await tempProvider.getBalance(address);
+
+  const loadingSymbols = ['|', '/', '-', '\\'];
+  let index = 0;
+  const loadingInterval = setInterval(() => {
+    process.stdout.write(`\rMemeriksa saldo untuk ${address}... ${loadingSymbols[index]}`);
+    index = (index + 1) % loadingSymbols.length;
+  }, 200);
+
+  while (balance <= amountCheck) {
+    try {
+      await delay(120000);
+      balance = await tempProvider.getBalance(address);
+    } catch (error) {
+      if (isTimeoutError(error)) {
+        console.log(`Kesalahan timeout terjadi: ${error.message}`);
+        await changeRpc;
+        continue;
+      }
+      const errorMessage = `Kesalahan memeriksa saldo: ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      throw error;
+    }
+  }
+
+  clearInterval(loadingInterval);
+  process.stdout.write('\r');
+  console.log(`Pemeriksaan saldo selesai. Saldo: ${ethers.formatEther(balance)} ETH`);
+  return balance;
+}
+async function checkBalanceDeposit(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, tempProvider);
+  const address = await wallet.getAddress();
+  let balanceDeposit = await tempProvider.getBalance(address);
+
+  const loadingSymbols = ['|', '/', '-', '\\'];
+  let index = 0;
+  const loadingInterval = setInterval(() => {
+    process.stdout.write(`\rChecking balance for ${address}... ${loadingSymbols[index]}`);
+    index = (index + 1) % loadingSymbols.length;
+  }, 200);
+
+  while (balanceDeposit <= amountCheck) {
+    try {
+      await delay(5000);
+      balanceDeposit = await tempProvider.getBalance(address);
+    } catch (error) {
+      if (isTimeoutError(error)) {
+        console.log(`Kesalahan timeout terjadi: ${error.message}`);
+        await changeRpc;
+        continue;
+      }
+      const errorMessage = `Kesalahan memeriksa saldo: ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      throw error;
+    }
+  }
+  clearInterval(loadingInterval);
+  process.stdout.write('\r');
+  console.log(`${ethers.formatEther(balanceDeposit)} ETH`.blue);
+  return balanceDeposit;
+}
+
+async function runWrapandUnwrap() {
+  displayHeader();
+  const timezone = moment().tz('Asia/Jakarta').format('HH:mm:ss [WIB] DD-MM-YYYY');
+  for (const PRIVATE_KEY of PRIVATE_KEYS) {
+    try {
+      let balance = await checkBalance(PRIVATE_KEY);
+      await delay(5000);
+      for (let i = 0; i < 15; i++) {
+        const txMessage = `Transaction Wrap and Unwrap`;
+        console.log(txMessage);
+        appendLog(txMessage);
+        let balanceDeposit = await checkBalanceDeposit(PRIVATE_KEY);
+        await delay(5000);
+        const receiptTx = await doWrap(PRIVATE_KEY);
+        if (receiptTx) {
+          const successMessage = `[${timezone}] Transaction Wrap: ${explorer.tx(receiptTx)}`;
+          console.log(successMessage.cyan);
+          appendLog(successMessage);
+        }
+        let balanceWeth = await checkWethBalance(PRIVATE_KEY);
+        await delay(5000);
+        const receiptTx2 = await doUnwrap(PRIVATE_KEY);
+        if (receiptTx2) {
+          const successMessage = `[${timezone}] Transaction Unwrap: ${explorer.tx(receiptTx2)}`;
+          console.log(successMessage.cyan);
+          appendLog(successMessage);
+        }
+      }
+      await delay(5000);
+      const sendMessage = `Transaction Send ETH`;
+      console.log(sendMessage);
+      appendLog(sendMessage);
+      let balanceSend = await checkBalanceDeposit(PRIVATE_KEY);
+      await delay(5000);
+      const receiptTxSend = await doSendEther(PRIVATE_KEY);
+      if (receiptTxSend) {
+        const successMessage = `[${timezone}] Transaction Send ETH: ${explorer.tx(receiptTxSend)}`;
+        console.log(successMessage.cyan);
+        appendLog(successMessage);
+      }
+      console.log('');
+    } catch (error) {
+      const errorMessage = `[${timezone}] Error processing transaction. Details: ${error.message}`;
+      console.log(errorMessage.red);
+      console.log(error);
+      appendLog(errorMessage);
+      await delay(5000);
+      console.log('');
+    }
+  }
+ }
+const job = new CronJob('0 10 * * *', async () => {
+  console.log('Transaksi akan dijalankan setiap )10:00 UTC');
+  await runWrapandUnwrap();
+});
+job.start();
+runWrapandUnwrap();

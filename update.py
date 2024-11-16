624e64787d37e04ca158456c66a5a0b442840d9d
Navira Gayatri <141285340+gnavira@users.noreply.github.com>
2024-11-16 03:05:30 +0000

    Fix Nonce modified:   firstrun/1/start.js modified:   firstrun/10/start.js modified:   firstrun/11/start.js modified:   firstrun/12/start.js modified:   firstrun/13/start.js modified:   firstrun/14/start.js modified:   firstrun/15/start.js modified:   firstrun/16/start.js modified:   firstrun/17/start.js modified:   firstrun/18/start.js modified:   firstrun/19/start.js modified:   firstrun/2/start.js modified:   firstrun/20/start.js modified:   firstrun/21/start.js modified:   firstrun/22/start.js modified:   firstrun/23/start.js modified:   firstrun/24/start.js modified:   firstrun/25/start.js modified:   firstrun/26/start.js modified:   firstrun/27/start.js modified:   firstrun/28/start.js modified:   firstrun/29/start.js modified:   firstrun/3/start.js modified:   firstrun/30/start.js modified:   firstrun/31/start.js modified:   firstrun/32/start.js modified:   firstrun/33/start.js modified:   firstrun/34/start.js modified:   firstrun/35/start.js modified:   firstrun/36/start.js modified:   firstrun/37/start.js modified:   firstrun/38/start.js modified:   firstrun/4/start.js modified:   firstrun/5/start.js modified:   firstrun/6/start.js modified:   firstrun/7/start.js modified:   firstrun/8/start.js modified:   firstrun/9/start.js modified:   notrun/1/start.js modified:   notrun/10/start.js modified:   notrun/11/start.js modified:   notrun/12/start.js modified:   notrun/13/start.js modified:   notrun/14/start.js modified:   notrun/15/start.js modified:   notrun/16/start.js modified:   notrun/17/start.js modified:   notrun/18/start.js modified:   notrun/19/start.js modified:   notrun/2/start.js modified:   notrun/20/start.js modified:   notrun/21/start.js modified:   notrun/22/start.js modified:   notrun/23/start.js modified:   notrun/24/start.js modified:   notrun/25/start.js modified:   notrun/26/start.js modified:   notrun/27/start.js modified:   notrun/28/start.js modified:   notrun/29/start.js modified:   notrun/3/start.js modified:   notrun/30/start.js modified:   notrun/31/start.js modified:   notrun/32/start.js modified:   notrun/33/start.js modified:   notrun/34/start.js modified:   notrun/35/start.js modified:   notrun/36/start.js modified:   notrun/37/start.js modified:   notrun/38/start.js modified:   notrun/4/start.js modified:   notrun/5/start.js modified:   notrun/6/start.js modified:   notrun/7/start.js modified:   notrun/8/start.js modified:   notrun/9/start.js modified:   update.py

--- update.py
+++ update.py
@@ -4,30 +4,241 @@ import os
 base_dirs = ["notrun", "firstrun"]
 
 # Kode lama yang ingin diganti
-
-# Kode baru yang akan menggantikan kode lama
-new_code = """async function getRoundedGasPrice(provider, defaultGasPrice) {
-  try {
-    // Mendapatkan data gas fee
-    const feeData = await provider.getFeeData();
-    let gasPrice = feeData.gasPrice;
-    if (!gasPrice) throw new Error("Gas price tidak tersedia");
-    let gasPriceInGwei = parseFloat(ethers.formatUnits(gasPrice, "gwei"));
-    if (gasPriceInGwei < 0.15) {
-      gasPriceInGwei = 0.15;
-    } else {
-      gasPriceInGwei = Math.ceil(gasPriceInGwei * 100) / 100;
+old_code = """async function doWrap(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const amount = ethers.parseUnits('1.5', 'ether');
+  const maxRetries = 3;
+  let attempt = 0;
+  const address = await wallet.getAddress();
+  while (attempt < maxRetries) {
+    try {
+	  const nonce = await provider.getTransactionCount(address, "latest");
+	  console.log(`Nonce: ${nonce}`);
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const wrapContract = new ethers.Contract(WETH_CA, ABI, wallet);
+      const txWrap = await wrapContract.deposit({ value: amount, gasPrice: gasPrice });
+      const receipt = await txWrap.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Wrap transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
     }
-    const getRoundedGasPrice = ethers.parseUnits(gasPriceInGwei.toString(), "gwei");
-    console.log(`Gas price: ${gasPriceInGwei} gwei`);
-    return getRoundedGasPrice;
-
-  } catch (error) {
-    console.log(`Error mendapatkan gas price: ${error.message}. Menggunakan default gas price ${ethers.formatUnits(defaultGasPrice, "gwei")} gwei`);
-    return defaultGasPrice;
   }
+
+  throw new Error(`Exceeded maximum retries for Wrap transaction.`);
 }
-"""
+
+async function doUnwrap(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const amount = ethers.parseUnits('1.5', 'ether');
+  const maxRetries = 3;
+  let attempt = 0;
+  const address = await wallet.getAddress();
+  while (attempt < maxRetries) {
+    try {
+	  const nonce = await provider.getTransactionCount(address, "latest");
+	  console.log(`Nonce: ${nonce}`);
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const unwrapContract = new ethers.Contract(WETH_CA, ABI, wallet);
+      const txUnwrap = await unwrapContract.withdraw(amount, { gasPrice: gasPrice });
+      const receipt = await txUnwrap.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Unwrap transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Unwrap transaction.`);
+}
+
+async function doSendEther(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const recipients = recipientsaddress;
+  const values = recipients.map(() => ethers.parseUnits('1.5', 'ether'));
+  const sendContract = new ethers.Contract(SEND_CA, SEND_ABI, wallet);
+  const maxRetries = 3;
+  let attempt = 0;
+  const address = await wallet.getAddress();
+  while (attempt < maxRetries) {
+    try {
+	  const nonce = await provider.getTransactionCount(address, "latest");
+	  console.log(`Nonce: ${nonce}`);
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const txSendContract = await sendContract.multicall(recipients, values, { value: ethers.parseUnits('1.5', 'ether'), gasPrice: gasPrice });
+      const receipt = await txSendContract.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Send ETH transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Send ETH transaction.`);
+}"""
+# Kode baru yang akan menggantikan kode lama
+new_code = """async function doWrap(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const amount = ethers.parseUnits('1.5', 'ether');
+  const maxRetries = 3;
+  let attempt = 0;
+  const address = await wallet.getAddress();
+  while (attempt < maxRetries) {
+    try {
+	  const nonce = await provider.getTransactionCount(address, "latest");
+	  console.log(`Nonce: ${nonce}`);
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const wrapContract = new ethers.Contract(WETH_CA, ABI, wallet);
+      const txWrap = await wrapContract.deposit({ value: amount, gasPrice: gasPrice, nonce: nonce});
+      const receipt = await txWrap.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Wrap transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Wrap transaction.`);
+}
+
+async function doUnwrap(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const amount = ethers.parseUnits('1.5', 'ether');
+  const maxRetries = 3;
+  let attempt = 0;
+  const address = await wallet.getAddress();
+  while (attempt < maxRetries) {
+    try {
+	  const nonce = await provider.getTransactionCount(address, "latest");
+	  console.log(`Nonce: ${nonce}`);
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const unwrapContract = new ethers.Contract(WETH_CA, ABI, wallet);
+      const txUnwrap = await unwrapContract.withdraw(amount, { gasPrice: gasPrice, nonce: nonce});
+      const receipt = await txUnwrap.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Unwrap transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Unwrap transaction.`);
+}
+
+async function doSendEther(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const recipients = recipientsaddress;
+  const values = recipients.map(() => ethers.parseUnits('1.5', 'ether'));
+  const sendContract = new ethers.Contract(SEND_CA, SEND_ABI, wallet);
+  const maxRetries = 3;
+  let attempt = 0;
+  const address = await wallet.getAddress();
+  while (attempt < maxRetries) {
+    try {
+	  const nonce = await provider.getTransactionCount(address, "latest");
+	  console.log(`Nonce: ${nonce}`);
+      const gasPrice = await getRoundedGasPrice(provider, defaultgasPrice);
+      const txSendContract = await sendContract.multicall(recipients, values, { value: ethers.parseUnits('1.5', 'ether'), gasPrice: gasPrice, nonce: nonce });
+      const receipt = await txSendContract.wait(1);
+      return receipt.hash;
+    } catch (error) {
+      attempt++;
+      const errorMessage = `[$timezone] Error executing Send ETH transaction (Attempt ${attempt}/${maxRetries}): ${error.message}`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      if (attempt < maxRetries && (error.message.includes('insufficient funds') || 
+          error.message.includes('nonce has already') ||
+          error.message.includes('Gateway Timeout') ||
+          error.message.includes('failed to detect network') ||
+		  error.message.includes('request timeout') ||
+		  error.message.includes('free limit') ||
+          error.message.includes('missing revert data'))) {
+        console.log(`Retrying transaction after delay...`);
+        await delay(20000);
+        await changeProvider();
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  throw new Error(`Exceeded maximum retries for Send ETH transaction.`);
+}"""
 
 # Fungsi untuk mencari dan mengganti kode di file
 def replace_code_in_file(file_path, old_code, new_code):
